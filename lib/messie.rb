b852716af609a0f449b8d5f623edb1f739ee1258
Dominik Liebler <liebler.dominik@googlemail.com>
2013-03-09 15:27:37 +0100

    refactored module loading

--- lib/messie.rb
+++ lib/messie.rb
@@ -16,56 +16,18 @@ module Messie
   VERSION = ::File.read(PATH + 'version.txt').strip
   # :startdoc:
 
-  # Returns the library path for the module. If any arguments are given,
-  # they will be joined to the end of the libray path using
-  # <tt>File.join</tt>.
-  #
-  def self.libpath(*args)
-    rv =  args.empty? ? LIBPATH : ::File.join(LIBPATH, args.flatten)
-
-    if block_given?
-      begin
-        $LOAD_PATH.unshift LIBPATH
-        rv = yield
-      ensure
-        $LOAD_PATH.shift
-      end
-    end
-    return rv
-  end
-
-  # Internal: Returns the lpath for the module. If any arguments are given,
-  # they will be joined to the end of the path using
-  # <tt>File.join</tt>.
-  #
-  # Returns: a String with the path for the module
-  def self.path(*args)
-    rv = args.empty? ? PATH : ::File.join(PATH, args.flatten)
-    if block_given?
-      begin
-        $LOAD_PATH.unshift PATH
-        rv = yield
-      ensure
-        $LOAD_PATH.shift
-      end
-    end
-
-    rv
-  end
-
   # Internal: Utility method used to require all files ending in .rb that lie in the
   # directory below this file that has the same name as the filename passed
   # in. Optionally, a specific _directory_ name can be passed in such that
   # the _filename_ does not have to be equivalent to the directory.
   #
   # Returns: an Array of files that have been require'd
-  def self.require_all_libs_relative_to(fname, dir = nil)
-    dir ||= ::File.basename(fname, '.*')
-    search_me = ::File.expand_path(
-        ::File.join(::File.dirname(fname), dir, '**', '*.rb'))
-
-    Dir.glob(search_me).sort.each {|rb| require rb}
+  def self.require_all_libs
+    base_path = File.dirname(__FILE__)
+    
+    files = %w(page request response string user_agent encoding/deflate encoding/gzip encoding/plain)
+    files.each {|f| require File.join(base_path, 'messie', f) }
   end
 end
 
-Messie.require_all_libs_relative_to(__FILE__)
+Messie.require_all_libs
